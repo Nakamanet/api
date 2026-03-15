b18f12ba1885867d560b042a3b357400da52806d
yumetia <yumetia000@gmail.com>
2026-03-15 07:25:27 -0400

    chore: models structured, routes/controllers  wip

--- app/models/anime.ts
+++ app/models/anime.ts
@@ -1,10 +1,14 @@
 import { BaseModel, column } from '@adonisjs/lucid/orm'
+import { DateTime } from 'luxon'
 
 export default class Anime extends BaseModel {
   public static table = 'Anime'
 
   @column({ isPrimary: true })
   declare id: number
+
+  @column()
+  declare slug: string
 
   @column()
   declare title_en: string
@@ -14,4 +18,43 @@ export default class Anime extends BaseModel {
 
   @column()
   declare synopsis: string
+
+  @column()
+  declare type: string
+
+  @column()
+  declare subtype: string
+
+  @column()
+  declare status: string
+
+  @column.date()
+  declare start_date: DateTime
+
+  @column.date()
+  declare end_date: DateTime
+
+  @column()
+  declare nsfw: boolean
+
+  @column()
+  declare poster_image: string
+
+  @column()
+  declare cover_image: string
+
+  @column()
+  declare age_rating: string
+
+  @column()
+  declare episode_count: number
+
+  @column()
+  declare episode_length: number
+
+  @column.dateTime({ autoCreate: true })
+  declare created_at: DateTime
+
+  @column.dateTime({ autoCreate: true, autoUpdate: true })
+  declare updated_at: DateTime
 }
--- app/models/manga.ts
+++ app/models/manga.ts
@@ -1,6 +1,7 @@
 import { BaseModel, column } from '@adonisjs/lucid/orm'
+import { DateTime } from 'luxon'
 
-export default class Anime extends BaseModel {
+export default class Manga extends BaseModel {
   public static table = 'Manga'
 
   @column({ isPrimary: true })
@@ -14,4 +15,32 @@ export default class Anime extends BaseModel {
 
   @column()
   declare synopsis: string
+
+  @column()
+  declare type: string
+
+  @column()
+  declare status: string
+
+  @column()
+  declare volume_count: number
+
+  @column()
+  declare chapter_count: number
+
+  @column.date()
+  declare start_date: DateTime
+
+  @column.date()
+  declare end_date: DateTime
+
+  @column()
+  declare poster_image: string
+
+  @column()
+  declare cover_image: string
+
+  @column.dateTime({ autoCreate: true })
+  declare created_at: DateTime
 }
+
